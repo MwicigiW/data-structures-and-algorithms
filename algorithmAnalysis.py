a5fc65749f9998fe8e4ed8f2ea1282e4e6bc1f02
MwicigiW <116628279+MwicigiW@users.noreply.github.com>
2023-05-26 15:26:35 -0400

    Update and rename algorithmAnalysis to algorithmAnalysis.py

--- algorithmAnalysis.py
+++ algorithmAnalysis.py
@@ -1,3 +1,4 @@
+'''
 What are data structures and algorithms?
   - Data structure = systematic way of organizing and accessing data
   - Algorithm = step-by-step procedure for performing some task in a finite amount of time. 
@@ -9,12 +10,19 @@ with n being the number of inputs.
 
 For simplicity, we will always assume worst-case time.
 
-```ruby
-require 'redcarpet'
-markdown = Redcarpet.new("Hello World!")
-puts markdown.to_html
-```
+'''
+def prefix_average1(S):
+    """Return list such that, for all j, A[j] equals average of S[0], ..., S[j]."""
+    n = len(S)
+    A = [0] * n  # create new list of n zeros
+    for j in range(n):
+        total = 0  # begin computing S[0] + ... + S[j]
+        for i in range(j + 1):
+            total += S[i]
+        A[j] = total / (j + 1)  # record the average
+    return A
 
+'''
 Functions
 Constant = (O)1
 Logarithmic = (O)log n 
@@ -30,4 +38,12 @@ Exponentials
 Big O notation
 Big Omega
 Big Theta
+
+Justification Techniques
+  - By Example
+  - Contra Attack
+  - Induction and Loop Invariants
+  - 
+  
+
 
